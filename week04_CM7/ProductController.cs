8a8e848dccded744013afb59e46fc0e3038485e6
林承美 <91170746+choww26@users.noreply.github.com>
2024-10-01 15:14:15 +0800

    week04_CM7

---
+++ week04_CM7/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : ApiController
+    {
+        fruit[] MyFruit = new fruit[]
+        {
+            new fruit {Id = 1, Name = "香蕉", Price = 30, Weight=5.2},
+            new fruit {Id = 2, Name = "鳳梨", Price=50, Weight=3.6},
+            new fruit {Id = 3, Name = "蘋果", Price=40, Weight=4.2}
+        };
+        public IHttpActionResult Get(int id)
+        {
+            var X = MyFruit.FirstOrDefault((p) => p.Id == id);
+            if (X == null) { return NotFound(); }
+            else { return Ok(X); }
+        }
+        public IEnumerable<fruit> Get()
+        {
+            return (MyFruit);
+        }
+    }
+}
---
+++ week04_CM7/fruit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class fruit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public double Weight {  get; set; }
+    }
+}
